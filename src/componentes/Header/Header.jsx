9ed6c75f3b6c739217d3ec7825675c8a562c39f3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 11:15:57 -0300

    teste

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return ( 
+    <>
+    <header className="bg-slate-500 flex justify-between  text-white p-4">
+        <span className="text-2xl"><Link to="/">CineTui</Link></span>
+        <Navbar/>
+    </header>
+    </> 
+    
+    );
+}
+
+export default Header;
